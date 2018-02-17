9970188d58971488aa4856153ab5e528ab663f44
Gu3r0 <jaime_rodriguez_5@hotmail.com>
2018-02-17 15:16:05 -0400

    Primera Subida

---
+++ Proyecto_1.Designer.vb
@@ -0,0 +1,356 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.TextBox5 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.Label9 = New System.Windows.Forms.Label
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label10 = New System.Windows.Forms.Label
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Placa"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Marca de Carro"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cedula de Dueño"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(11, 187)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nombre de Dueño"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(79, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(178, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Registro de Entrada de Carros"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 213)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(94, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Apellido de Dueño"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(114, 75)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(114, 158)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(114, 184)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(114, 210)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox5.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(11, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "<-Anterior"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(108, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Siguiente->"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(8, 57)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Cargar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.LightGray
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(264, 319)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Finalizar"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(250, 202)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(89, 94)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Archivo"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(8, 28)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "Guardar"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.LightGray
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button6.Location = New System.Drawing.Point(183, 319)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 16
+        Me.Button6.Text = "Aceptar"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Location = New System.Drawing.Point(292, 78)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 15)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Label7"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(18, 242)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(216, 54)
+        Me.GroupBox2.TabIndex = 18
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Control"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(17, 329)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "Label8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(63, 9)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(216, 20)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "Multinacional de Seguros "
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1500
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Dodge", "Chevrolet", "Ford", "Nissan", "Hyundai", "Volvo", "Mazda", "Fiat", "Kia"})
+        Me.ComboBox1.Location = New System.Drawing.Point(114, 101)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(122, 21)
+        Me.ComboBox1.TabIndex = 21
+        Me.ComboBox1.Text = "Selecione Marca"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(13, 131)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(80, 13)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = "Marca de Carro"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(114, 131)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox1.TabIndex = 23
+        Me.CheckBox1.Text = "Automatico"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(201, 131)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(76, 17)
+        Me.CheckBox2.TabIndex = 24
+        Me.CheckBox2.Text = "Sincronico"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(351, 353)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Registro de Entrada de Carros"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Proyecto_1.vb
@@ -0,0 +1,250 @@
+﻿Public Class Form1
+    Private Structure DatStruct
+        Public Placa As String
+        Public Marca_carro As String
+        Public Cedula As Long
+        Public Nombre As String
+        Public Apellido As String
+        Public Tipo_carro As String
+    End Structure
+    Dim Datpers() As DatStruct
+    Dim Index As Int32
+    Sub limpiador()
+        TextBox1.Text = ""
+        ComboBox1.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label7.Text = ""
+        Label8.Text = ""
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Label8.Text = ""
+        Timer1.Enabled = False
+    End Sub
+    Private Sub Label8_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Label8.TextChanged
+        Timer1.Enabled = True
+    End Sub
+    'Aceptar
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If TextBox1.Text = "" Or ComboBox1.Text = "" Or TextBox3.Text = "" Or TextBox4.Text = "" Or TextBox5.Text = "" Then
+            MessageBox.Show("NO SE PUEDEN DEJAR LOS CAMPOS EN BLANCO")
+        Else
+            Try
+                ReDim Preserve Datpers(UBound(Datpers) + 1)
+            Catch ex As Exception
+                ReDim Preserve Datpers(1)
+            End Try
+            Index = UBound(Datpers)
+            Label7.Text = UBound(Datpers)
+            With Datpers(UBound(Datpers))
+                .Placa = TextBox1.Text
+                .Marca_carro = ComboBox1.Text
+                .Cedula = TextBox3.Text
+                .Nombre = TextBox4.Text
+                .Apellido = TextBox5.Text
+                .Tipo_carro = CheckBox1.Text
+                .Tipo_carro = CheckBox2.Text
+                Label8.Text = "¡Datos almacenados con éxito!"
+                Call limpiador()
+                TextBox1.Focus()
+
+            End With
+
+        End If
+    End Sub
+    'Anterior
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Index > 1 Then
+            Index -= 1
+            Label7.Text = Index & " / " & UBound(Datpers)
+            With Datpers(Index)
+                TextBox1.Text = .Placa
+                ComboBox1.Text = .Marca_carro
+                TextBox3.Text = .Cedula
+                TextBox4.Text = .Nombre
+                TextBox5.Text = .Apellido
+                .Tipo_carro = CheckBox1.Text
+                .Tipo_carro = CheckBox2.Text
+            End With
+        Else
+            Label8.Text = "¡Inicio de Archivo Encontrado!"
+            Beep()
+        End If
+    End Sub
+    'Siguiente
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Index < UBound(Datpers) Then
+            Index += 1
+            Label7.Text = Index & " / " & UBound(Datpers)
+            With Datpers(Index)
+                TextBox1.Text = .Placa
+                ComboBox1.Text = .Marca_carro
+                TextBox3.Text = .Cedula
+                TextBox4.Text = .Nombre
+                TextBox5.Text = .Apellido
+                CheckBox1.Text = .Tipo_carro
+                CheckBox2.Text = .Tipo_carro
+            End With
+        Else
+            Label8.Text = "¡Final de Archivo Encontrado!"
+            Beep()
+        End If
+    End Sub
+    'Salvar
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim FleNme As String = Mid(Application.ExecutablePath, 1, InStrRev(Application.ExecutablePath, "\")) & "Datpers.txt"
+        FileOpen(1, FleNme, OpenMode.Binary)
+        FilePut(1, UBound(Datpers))
+        FilePut(1, Datpers)
+        FileClose(1)
+    End Sub
+    'Cargar
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim FleNme As String = Mid(Application.ExecutablePath, 1, InStrRev(Application.ExecutablePath, "\")) & "Datpers.Txt"
+        FileOpen(1, FleNme, OpenMode.Binary)
+        FileGet(1, Index)
+        ReDim Datpers(Index)
+        FileGet(1, Datpers)
+        FileClose(1)
+        Label7.Text = UBound(Datpers)
+    End Sub
+    'Finalizar
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        End
+    End Sub
+    'Caja de texto de PLACA
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
+        Select Case Asc(e.KeyChar)
+            Case Is = 8
+            Case Is = 13
+                ComboBox1.Focus()
+            Case Is = 27
+            Case Else
+                If InStr("1234567890ABCDEFGHIJKLMNÑPQRSTVYXZ", UCase(e.KeyChar)) = 0 Then
+                    Beep()
+                    e.Handled = True
+                Else : e.KeyChar = UCase(e.KeyChar)
+                End If
+        End Select
+    End Sub
+
+    Private Sub TextBox2_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs)
+        Select Case Asc(e.KeyChar)
+            Case Is = 8
+            Case Is = 13 'ENTER
+                TextBox3.Focus()
+            Case Is = 27 'ESC
+                TextBox1.Focus()
+            Case Else
+                If InStr("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ '.", UCase(e.KeyChar)) = 0 Then
+                    Beep()
+                    e.Handled = True
+                Else
+                    e.KeyChar = UCase(e.KeyChar)
+                End If
+        End Select
+    End Sub
+
+    Private Sub TextBox3_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox3.KeyPress
+        Select Case Asc(e.KeyChar)
+            Case Is = 8
+            Case Is = 13
+                TextBox4.Focus()
+            Case Is = 27
+                ComboBox1.Focus()
+            Case Else
+                If InStr("1234567890", e.KeyChar) = 0 Then
+                    Beep()
+                    e.Handled = True
+                End If
+        End Select
+    End Sub
+
+    Private Sub TextBox4_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox4.KeyPress
+        Select Case Asc(e.KeyChar)
+            Case Is = 8
+            Case Is = 13
+                TextBox5.Focus()
+            Case Is = 27
+                TextBox3.Focus()
+            Case Else
+                If InStr("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ '.", UCase(e.KeyChar)) = 0 Then
+                    Beep()
+                    e.Handled = True
+                Else
+                    If TextBox4.SelectionStart = 0 Then
+                        e.KeyChar = UCase(e.KeyChar)
+                    Else
+                        If Mid(TextBox4.Text, TextBox4.SelectionStart, 1) = " " Then
+                            e.KeyChar = UCase(e.KeyChar)
+                        Else
+                            e.KeyChar = LCase(e.KeyChar)
+                        End If
+                    End If
+                End If
+        End Select
+    End Sub
+
+    Private Sub TextBox5_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox5.KeyPress
+        Select Case Asc(e.KeyChar)
+            Case Is = 8
+            Case Is = 13
+                Button6.Focus()
+            Case Is = 27
+                TextBox3.Focus()
+            Case Else
+                If InStr("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ '.", UCase(e.KeyChar)) = 0 Then
+                    Beep()
+                    e.Handled = True
+                Else
+                    If TextBox5.SelectionStart = 0 Then
+                        e.KeyChar = UCase(e.KeyChar)
+                    Else
+                        If Mid(TextBox5.Text, TextBox5.SelectionStart, 1) = " " Then
+                            e.KeyChar = UCase(e.KeyChar)
+                        Else
+                            e.KeyChar = LCase(e.KeyChar)
+                        End If
+                    End If
+                End If
+        End Select
+    End Sub
+
+
+    Private Sub TextBox1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox1.MouseDown
+        Clipboard.Clear()
+        Clipboard.SetText(" ")
+    End Sub
+    Private Sub TextBox2_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs)
+        Clipboard.Clear()
+        Clipboard.SetText(" ")
+    End Sub
+    Private Sub TextBox3_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox3.MouseDown
+        Clipboard.Clear()
+        Clipboard.SetText(" ")
+    End Sub
+    Private Sub TextBox4_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox4.MouseDown
+        Clipboard.Clear()
+        Clipboard.SetText(" ")
+    End Sub
+    Private Sub TextBox5_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox5.MouseDown
+        Clipboard.Clear()
+        Clipboard.SetText(" ")
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox2.Checked = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox1.Checked = False
+        End If
+    End Sub
+End Class
